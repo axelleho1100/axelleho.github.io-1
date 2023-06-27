97514ca1d3642f6ce50ad9c3d7b5d62a912ae1cf
axelleho <68615983+axelleho@users.noreply.github.com>
2023-06-26 20:27:12 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style=" color: #af4b80; "> 3D Modeller, Animator, VFX Artist and Front End Designer </span>')
+  .pauseFor(2000)
+  .deleteChars(10)
+  .start();
